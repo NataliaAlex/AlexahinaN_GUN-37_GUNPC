4859dfb5385d42bf56e7a86dfb96e359cfbb7643
Natalie <nataliealexahina@gmail.com>
2025-06-21 19:05:25 +0300

    Циклы

---
+++ Циклы/Циклы/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Циклы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //С помощью цикла for (или while) выведите первые 10 чисел Фиббоначи
+            Console.WriteLine("Задание 1");
+            int[] array1 = new [] { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
+            for (int index = 0; index < array1.Length; index++)
+            {
+                Console.WriteLine(array1[index]);
+            }
+
+            //Используя цикл for, выведите все чётные числа от 2 до 20
+            Console.WriteLine();
+            Console.WriteLine("Задание 2");
+            for (int i = 2; i <= 20; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+
+            //С помощью вложенных циклов for выведите таблицу умножения от 1 до 5
+            //Каждая строка таблицы должна быть выведена в отдельной строке
+            Console.WriteLine();
+            Console.WriteLine("Задание 3");
+            
+            for (int j = 1; j <= 5; j++)
+            {
+                for (int k = 0; k <= 10; k++)
+                {
+                    Console.WriteLine(j*k);
+                }
+                Console.WriteLine();
+            }
+          
+            //Дана строка string password = “qwerty”;
+            //Напишите программу для ввода пароля, которая считывает пользовательский ввод Console.ReadLine
+            //Подсказка: используйте do -while
+            Console.WriteLine();
+            Console.WriteLine("Задание 4");
+            Console.WriteLine("Enter the password (*6)");
+            string password = "qwerty";
+            var letters = "";
+            do
+            {
+                letters = Console.ReadLine();
+                if (letters != password)
+                {
+                    Console.WriteLine("Wrong password");
+                }
+            }
+            while (letters != password);
+            Console.WriteLine("Correct password");
+         }
+    }
+}
---
+++ Циклы/Циклы/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Циклы")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Циклы")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("60287aa4-1804-43d7-a733-99bab1d075fb")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
